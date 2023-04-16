c1751138761835ad83889df2475673a6936dc663
Dave Isaías Pramax <davepramax16@gmail.com>
2023-04-16 13:31:04 -0600

    Agregando ejercicios del Quinto Codelab

---
+++ EjercicioFinal.dart
@@ -0,0 +1,35 @@
+
+Future<String> fetchUsername() async {
+  return Future.delayed(const Duration(seconds: 3), () => 'DavePravia');
+}
+
+Future<String> logoutUser() async {
+  var username = await fetchUsername();
+  print('Se ha cerrado sesion ... adios');
+  return 'Usuario desconectado: $username';
+}
+
+String addHello(String user) => 'Hello $user';
+
+Future<String> greetUser() async {
+  var username = await fetchUsername();
+  return addHello(username);
+}
+
+Future<String> sayGoodbye() async {
+  try {
+    var result = await logoutUser();
+    return '$result Thanks, see you next time';
+  } catch (e) {
+    return 'Failed to logout user: $e';
+  }
+}
+
+void main() async {
+
+  String GreetUser = await greetUser();
+  print(GreetUser);
+
+  String Despedida = await sayGoodbye();
+  print(Despedida);
+}
